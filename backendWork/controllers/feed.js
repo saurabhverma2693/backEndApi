849d3efcb2afd809e0fd724443d4d51ea446da34
saurabh <saurabhverma2693@gmail.com>
2025-06-17 18:16:06 +0530

    Add Pagination

--- backendWork/controllers/feed.js
+++ backendWork/controllers/feed.js
@@ -5,12 +5,27 @@ const { validationResult } = require('express-validator/check');
 
 const Post = require('../models/post');
 
+
 exports.getPosts = (req, res, next) => {
+  const currentPage = req.query.page || 1;
+  const perPage = 2;
+  let totalItems;
   Post.find()
+    .countDocuments()
+    .then(count => {
+      totalItems = count;
+      return Post.find()
+        .skip((currentPage - 1) * perPage)
+        .limit(perPage);
+    })
     .then(posts => {
       res
         .status(200)
-        .json({ message: 'Fetched posts successfully.', posts: posts });
+        .json({
+          message: 'Fetched posts successfully.',
+          posts: posts,
+          totalItems: totalItems
+        });
     })
     .catch(err => {
       if (!err.statusCode) {
@@ -19,6 +34,7 @@ exports.getPosts = (req, res, next) => {
       next(err);
     });
 };
+
 
 exports.createPost = (req, res, next) => {
   const errors = validationResult(req);
