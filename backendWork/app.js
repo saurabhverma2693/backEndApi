9bdac6113e4d342eec5986562e4c774913add534
saurabh <saurabhverma2693@gmail.com>
2025-06-18 14:47:04 +0530

    Sign Up Form and Validations

--- backendWork/app.js
+++ backendWork/app.js
@@ -8,6 +8,7 @@ const fs = require('fs');
 
 
 const feedRoutes = require('./routes/feed');
+const authRoutes = require('./routes/auth');
 
 const app = express();
 
@@ -56,12 +57,14 @@ app.use((req, res, next) => {
 });
 
 app.use('/feed', feedRoutes);
+app.use('/auth', authRoutes);
 
 app.use((error, req, res, next) => {
   console.log(error);
   const status = error.statusCode || 500;
   const message = error.message;
-  res.status(status).json({ message: message });
+  const data = error.data;
+  res.status(status).json({ message: message, data: data });
 });
 
 mongoose
